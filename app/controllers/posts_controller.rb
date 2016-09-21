e1e43f85a90097e532dd4d695ed370b530ad77a4
Fitzonreee <kevin.fitzhenry@createthenext.com>
2016-09-21 15:45:42 -0700

    Added the three steps to using AJAX: added remote:true to form_for, added format.js in the create action in posts controller, and created a js file in the posts directory with the same name as the action in the controller - create

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -32,9 +32,11 @@ class PostsController < ApplicationController
         # format.html { redirect_to @post, notice: 'Post was successfully created.' }
         format.html { redirect_to root_path, notice: 'Post was successfully created.' }
         format.json { render :show, status: :created, location: @post }
+        format.js
       else
         format.html { render :new }
         format.json { render json: @post.errors, status: :unprocessable_entity }
+        format.js
       end
     end
   end
